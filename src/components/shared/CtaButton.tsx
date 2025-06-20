644ed786ad821e5d1c467328823f29af889e44a0
imflawlezz <egorart6@gmail.com>
2025-06-20 19:46:03 +0200

    feat: add "Call to Action" button component with type definition

---
+++ src/components/shared/CtaButton.tsx
@@ -0,0 +1,14 @@
+'use client';
+export const CtaButton = ({
+  label,
+  onClick,
+  className
+}: CtaButtonProps) => {
+    return (
+        <button
+            className={`${className} text-white border px-12 py-1 font-cardo font-bold text-xl text-center hover:bg-white hover:text-gray-500 transition duration-200 ease-in-out`}
+            onClick={() => onClick}>
+                {label}
+        </button>
+    )
+}
