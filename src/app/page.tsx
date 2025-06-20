7e2be62bc419261a9fa3cfc50ad22c36a32e3bc1
imflawlezz <egorart6@gmail.com>
2025-06-20 19:47:14 +0200

    feat: add Hero.tsx component, adjust page.tsx layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import {Header} from "@/components/sections/Header";
+import {Hero} from "@/components/sections/Hero";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <Hero/>
+        </>
+    );
 }
