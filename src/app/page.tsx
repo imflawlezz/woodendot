edca124eafebc4e17856cf5b73c1594caf47ff57
imflawlezz <egorart6@gmail.com>
2025-06-23 01:51:07 +0200

    feat: add "Featured Furniture" section, adjust page.tsx layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import {Header} from "@/components/sections/Header";
 import {Hero} from "@/components/sections/Hero";
 import {Advantages} from "@/components/sections/Advantages";
 import {Categories} from "@/components/sections/Categories";
+import {FeaturedFurniture} from "@/components/sections/FeaturedFurniture";
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             <Hero/>
             <Advantages/>
             <Categories />
+            <FeaturedFurniture/>
         </>
     );
 }
---
+++ src/components/sections/FeaturedFurniture.tsx
@@ -0,0 +1,23 @@
+import {FurnitureCard} from "@/components/shared/FurnitureCard";
+import {FeaturedFurnitureList} from "@/data/FeaturedFurnitureList";
+
+export const FeaturedFurniture = () => {
+    return (
+        <section className="flex flex-col md:gap-y-7 lg:mt-28">
+            {FeaturedFurnitureList.map((item, index) => (
+                <FurnitureCard
+                    key={`${item.title}-${index}`}
+                    mark={item.mark}
+                    title={item.title}
+                    subtitle={item.subtitle}
+                    price={item.price}
+                    description={item.description}
+                    additions={item.additions}
+                    awards={item.awards}
+                    imageURL={item.imageURL}
+                    variant={index % 2 === 0 ? 'left' : 'right'}
+                />
+            ))}
+        </section>
+    )
+}
