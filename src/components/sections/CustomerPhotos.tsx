1431b8294ac4de4c5373e9c49a933b19f626fe24
imflawlezz <egorart6@gmail.com>
2025-06-24 20:35:57 +0200

    style: adjust vertical margins on smaller screen sizes, add horizontal padding to text block

--- src/components/sections/CustomerPhotos.tsx
+++ src/components/sections/CustomerPhotos.tsx
@@ -3,8 +3,8 @@ import {CustomerPhotosSlider} from "@/components/shared/CustomerPhotosSlider";
 
 export const CustomerPhotos = () => {
     return (
-        <section className="flex flex-col gap-5 md:gap-9 my-8 md:my-12">
-            <div className="font-cardo flex flex-col items-center justify-center gap-4 md:gap-5">
+        <section className="flex flex-col gap-5 md:gap-9 my-12">
+            <div className="font-cardo flex flex-col items-center justify-center text-center gap-4 md:gap-5 px-4 ">
                 <h1 className="font-bold text-2xl md:text-3xl">
                     Our pieces inside your home
                 </h1>
